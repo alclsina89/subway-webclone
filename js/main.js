a1e65924fdd44b1b0a1b209e75ee12316b1b8d7b
unknown <alclsina23@gmail.com>
2020-10-22 19:44:49 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-    alert(111);
+    // alert(111);
+    gnb();
 })
+
+function gnb() {
+    var spd = 0.5,
+        eft = Power4.easeOut;
+        $('#gnb >ul >li >.nv1').on('mouseenter', function(){
+            TweenLite.to($('header').css('overflow','inherit').addClass('open'), spd, {ease: eft, height: 425})
+            TweenLite.to($('.nv2'), spd, { ease: eft, top: 0, opacity:1})
+        })
+        $('#gnb').on('mouseleave', function () {
+            TweenLite.to($('header').css('overflow', 'hidden'), spd, {
+                ease: eft, height: 165, onComplete: function () {
+                    $('header').removeClass('open')
+                }
+            })
+            TweenLite.to($('.nv2'), spd, { ease: eft, top: -20, opacity: 0 })
+        })
+}
