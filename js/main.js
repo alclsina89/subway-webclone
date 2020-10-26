006499e3172a47953ee166beca69e95ca161cbc1
unknown <alclsina23@gmail.com>
2020-10-26 15:38:35 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -6,16 +6,17 @@ $(document).ready(function(){
 function gnb() {
     var spd = 0.5,
         eft = Power4.easeOut;
-        $('#gnb >ul >li >.nv1').on('mouseenter', function(){
-            TweenLite.to($('header').css('overflow','inherit').addClass('open'), spd, {ease: eft, height: 425})
-            TweenLite.to($('.nv2'), spd, { ease: eft, top: 0, opacity:1})
+    $('#gnb >ul >li >.nv1').on('mouseenter', function(){
+        TweenLite.to($('header').css('overflow','inherit').addClass('open'), spd, {ease: eft, height: 425})
+        TweenLite.to($('.nv2'), spd, { ease: eft, top: 0, opacity:1})
+    })
+    $('#gnb').on('mouseleave', function () {
+        TweenLite.to($('header').css('overflow', 'hidden'), spd, {
+            ease: eft, height: 165,
+            onComplete: function () {
+                $('header').removeClass('open')
+            }
         })
-        $('#gnb').on('mouseleave', function () {
-            TweenLite.to($('header').css('overflow', 'hidden'), spd, {
-                ease: eft, height: 165, onComplete: function () {
-                    $('header').removeClass('open')
-                }
-            })
-            TweenLite.to($('.nv2'), spd, { ease: eft, top: -20, opacity: 0 })
-        })
+        TweenLite.to($('.nv2'), spd, { ease: eft, top: -20, opacity: 0 })
+    })
 }
